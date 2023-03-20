f4e3fde9d7fe9d74b084e56d1ba01a2d6e2ee719
Илья Сысоев <123730648+Yudin-num2@users.noreply.github.com>
2023-03-20 16:46:21 +0300

    volatilitys with threads on OOP

--- lesson_012/02_volatility_with_threads.py
+++ lesson_012/02_volatility_with_threads.py
@@ -24,7 +24,7 @@ import os
 from operator import itemgetter
 from pprint import pprint
 import time
-from threading import Thread
+import threading
 def time_track(func):
     def surrogate(*args, **kwargs):
         started_at = time.time()
@@ -38,42 +38,65 @@ def time_track(func):
     return surrogate
 
 
-class Volater:
+class Volater(threading.Thread):
 
-    def __init__(self, path, *args, **kwargs):
-        # super().__init__(*args, **kwargs)
-        self.path = path
+    def __init__(self, f, lock, all_volatilitys, zero, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.f = f
         self.sorted_total_prices = []
-        self.all_volatilitys = {}
-        self.zero_volatility = {}
-        self.max = None
-        self.min = None
+        self.all_volatilitys = all_volatilitys
+        self.zero_volatility = zero
+        self.lock_all_volatilitys = lock
 
-    @time_track
     def run(self):
 
-        for dirname, dirpath, files in os.walk(self.path):
-            for file in files:
-                total_prices = []
-                with open(os.path.join(dirname, file), mode='r', encoding='utf8') as f:
-                    for line in f:
-                        if 'PRICE' not in line:
-                            price = float(line.split(',')[2])
-                            total_prices.append(price)
-                self.sorted_total_prices = sorted(total_prices)
-                average_price = (self.sorted_total_prices[-1] + self.sorted_total_prices[0]) / 2
-                volatility = round(((self.sorted_total_prices[-1] -
-                                     self.sorted_total_prices[0]) / average_price) * 100, 3)
-                if volatility == 0.0:
-                    self.zero_volatility[file] = volatility
-                else:
-                    self.all_volatilitys[file] = volatility
-                    self.max = sorted(self.all_volatilitys.items(), key=itemgetter(1))[-1:-4:-1]
-                    self.min = sorted(self.all_volatilitys.items(), key=itemgetter(1))[0:3]
-            print(f'Максимальная волатильность: \n {self.max}')
-            print(f'Минимальная волатильность: \n {self.min}')
-            print(f'Нулевая волатильность: \n {self.zero_volatility}')
+        with open(file=os.path.join('trades', self.f), mode='r', encoding='utf8') as file:
+
+            total_prices = []
+            for line in file:
+                if 'PRICE' not in line:
+                    price = float(line.split(',')[2])
+                    total_prices.append(price)
+        self.sorted_total_prices = sorted(total_prices)
+        average_price = (self.sorted_total_prices[-1] + self.sorted_total_prices[0]) / 2
+        volatility = round(((self.sorted_total_prices[-1] -
+                             self.sorted_total_prices[0]) / average_price) * 100, 3)
+        if volatility == 0.0:
+            with self.lock_all_volatilitys:
+                self.zero_volatility[self.f] = volatility
+        else:
+            with self.lock_all_volatilitys:
+                self.all_volatilitys[self.f] = volatility
 
 
-volater = Volater(path=r'C:\Users\i.sysoev\PycharmProjects\pythonProject2\lesson_012\trades')
-volater.run()
+lock = threading.Lock()
+all_volatilitys = {}
+zero = {}
+path_to_directory = 'trades'
+all_files = []
+
+@time_track
+def main():
+
+    for dirname, dirpath, files in os.walk(path_to_directory):
+        for file in files:
+            all_files.append(file)
+
+    volaters = [Volater(f=f, lock=lock, all_volatilitys=all_volatilitys, zero=zero) for f in all_files]
+
+    for volater in volaters:
+        volater.start()
+    for volater in volaters:
+        volater.join()
+
+    max_volatilitys = sorted(all_volatilitys.items(), key=itemgetter(1))[-1:-4:-1]
+    min_volatilitys = sorted(all_volatilitys.items(), key=itemgetter(1))[0:3]
+
+    print(f'Максимальная волатильность: \n {max_volatilitys}')
+    print(f'Минимальная волатильность: \n {min_volatilitys}')
+    print(f'Нулевая волатильность: \n {zero}')
+
+
+if __name__ == '__main__':
+    main()
+
